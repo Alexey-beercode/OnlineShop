86736a576ebccaa551477c0656afe10f3873965a
Alexey-beercode <alsemkovbn@gmail.com>
2024-06-19 09:46:31 +0300

    Added entities

--- OnlineShop/OnlineShop.DAL/Entities/Implementations/Product.cs
+++ OnlineShop/OnlineShop.DAL/Entities/Implementations/Product.cs
@@ -5,4 +5,9 @@ namespace OnlineShop.DAL.Entities.Implementations;
 public class Product:IEntity
 {
     public Guid Id { get; set; }
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public Guid CategoryId { get; set; }
+    public string Description { get; set; }
+    public bool IsDeleted { get; set; }
 }
