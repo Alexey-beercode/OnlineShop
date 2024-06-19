afdc64a5a7b9c6dad0980dff0c29104246f2ea12
Intensio <Alexander0aki@gmail.com>
2024-06-19 11:03:34 +0300

    Order entity rework

--- OnlineShop/OnlineShop.DAL/Entities/Implementations/Order.cs
+++ OnlineShop/OnlineShop.DAL/Entities/Implementations/Order.cs
@@ -6,9 +6,7 @@ public class Order : IEntity
 {
     public Guid Id { get; set; }
 
-    public required Guid UserId { get; set; }
     public required DateTime OrderDate { get; set; }
-    public required decimal TotalPrice { get; set; }
 
     public required User User { get; set; }
     public required ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
