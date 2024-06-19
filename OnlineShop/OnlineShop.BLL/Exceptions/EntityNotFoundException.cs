322ba995adfa87d7532df46c4ccf73337d50c052
Albertbuh <albertbuhovets@mail.ru>
2024-06-19 10:54:12 +0300

    Change exception namespace

--- OnlineShop/OnlineShop.BLL/Exceptions/EntityNotFoundException.cs
+++ OnlineShop/OnlineShop.BLL/Exceptions/EntityNotFoundException.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace OnlineShop.DAL.Exceptions
+namespace OnlineShop.BLL.Exceptions
 {
     public class EntityNotFoundException : ServiceException
     {
