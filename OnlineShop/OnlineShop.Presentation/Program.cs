42130a964c0f5d8f992b242fb2ddbdabeb256eaf
Albertbuh <albertbuhovets@mail.ru>
2024-06-20 20:40:39 +0300

    Init configuration to Programcs

--- OnlineShop/OnlineShop.Presentation/Program.cs
+++ OnlineShop/OnlineShop.Presentation/Program.cs
@@ -1,6 +1,46 @@
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using Mapster;
+using Microsoft.EntityFrameworkCore;
+using OnlineShop.BLL.Entities.Validators;
+using OnlineShop.BLL.Services.Implementations;
+using OnlineShop.BLL.Services.Interfaces;
+using OnlineShop.DAL.Infrastructure;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddControllers();
+
+builder.Services.AddFluentValidationAutoValidation().AddFluentValidationClientsideAdapters();
+//idk which way would be better (not sure that first is working in right way)
+//builder.Services.AddValidatorsFromAssembly(typeof(OrderItemValidator).Assembly);
+builder.Services.AddValidatorsFromAssemblyContaining<OrderItemValidator>();
+
+builder.Services.AddDbContext<ShopDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("PostgresDatabase")));
+
+builder.Services.AddSwaggerGen();
+
+//TODO: need to learn how it works
+TypeAdapterConfig.GlobalSettings.Scan(Assembly.GetExecutingAssembly());
+builder.Services.AddMapster();
+
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<IOrderService, OrderService>();
+builder.Services.AddScoped<IOrderItemService, OrderItemService>();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if(app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapControllers();
 
 app.Run();
