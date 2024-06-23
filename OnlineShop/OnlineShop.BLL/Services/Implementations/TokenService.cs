a5b91cbc3c0f86cc1337f3a90832379ba2cb73ea
Alexey-beercode <alsemkovbn@gmail.com>
2024-06-23 23:38:47 +0300

    Added token service, jwt bearer cofig

---
+++ OnlineShop/OnlineShop.BLL/Services/Implementations/TokenService.cs
@@ -0,0 +1,48 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using OnlineShop.BLL.Services.Interfaces;
+using OnlineShop.DAL.Entities.Implementations;
+
+namespace OnlineShop.BLL.Services.Implementations;
+
+public class TokenService:ITokenService
+{
+    private readonly IConfiguration _configuration;
+
+    public TokenService(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    private List<Claim> CreateClaims(User user)
+    {
+        var claims = new List<Claim>
+        {
+            new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+            new Claim(ClaimTypes.Name, user.FirstName),
+            new Claim(ClaimTypes.Surname,user.LastName),
+            new Claim(ClaimTypes.GivenName,user.Login)
+        };
+        return claims;
+    }
+    
+    public string GenerateAccessToken(User user)
+    {
+        var claims = CreateClaims(user);
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Secret"]));
+        var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        var jwtToken = new JwtSecurityToken(
+            issuer: _configuration["Jwt:Issuer"],
+            audience: _configuration["Jwt:Audience"],
+            claims: claims,
+            expires: DateTime.UtcNow.AddHours(int.Parse(_configuration["Jwt:Expire"])),
+            signingCredentials: credentials
+        );
+        var tokenHandler = new JwtSecurityTokenHandler();
+        return tokenHandler.WriteToken(jwtToken);
+    }
+}
---
+++ OnlineShop/OnlineShop.Presentation/Extensions/WebApplicationBuilderExtension.cs
@@ -0,0 +1,59 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.IdentityModel.Tokens;
+
+namespace OnlineShop.Extensions;
+
+public static class WebApplicationBuilderExtension
+{
+    public static void AddServices(this WebApplicationBuilder builder)
+    {
+
+        builder.Services.AddControllers();
+    }
+    
+
+    public static void AddDatabase(this WebApplicationBuilder builder)
+    {
+    }
+
+    public static void AddAuthentication(this WebApplicationBuilder builder)
+    {
+        var jwtSettings = builder.Configuration.GetSection("Jwt");
+
+        var key = Encoding.UTF8.GetBytes(jwtSettings["Secret"]);
+        var issuer = jwtSettings["Issuer"];
+        var audience = jwtSettings["Audience"];
+
+        builder.Services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(options =>
+        {
+            options.RequireHttpsMetadata = false;
+            options.SaveToken = true;
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = issuer,
+                ValidAudience = audience,
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+            };
+        });
+        builder.Services.AddAuthorization(options => options.DefaultPolicy =
+            new AuthorizationPolicyBuilder
+                    (JwtBearerDefaults.AuthenticationScheme)
+                .RequireAuthenticatedUser()
+                .Build());
+    }
+    
+    public static void AddSwaggerDocumentation(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+    }
+}
