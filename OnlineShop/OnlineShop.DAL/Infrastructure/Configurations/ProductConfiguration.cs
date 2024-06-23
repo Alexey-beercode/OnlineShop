96bfa948bb63b2b06d9f231198d9fc36ee19682e
Alexey-beercode <alsemkovbn@gmail.com>
2024-06-23 22:13:42 +0300

    Added validators, fluent config

---
+++ OnlineShop/OnlineShop.DAL/Infrastructure/Configurations/ProductConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OnlineShop.DAL.Entities.Implementations;
+
+namespace OnlineShop.DAL.Infrastructure.Configurations
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.ToTable("Products");
+
+            builder.HasKey(p => p.Id);
+
+            builder.Property(p => p.Id)
+                .IsRequired()
+                .ValueGeneratedOnAdd();
+
+            builder.Property(p => p.Name)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(p => p.Price)
+                .IsRequired();
+
+            builder.Property(p => p.Description)
+                .HasMaxLength(500);
+
+            builder.Property(p => p.IsDeleted)
+                .IsRequired();
+
+            builder.HasOne(p => p.Category)
+                .WithMany()
+                .HasForeignKey("CategoryId") // Use a shadow property
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
