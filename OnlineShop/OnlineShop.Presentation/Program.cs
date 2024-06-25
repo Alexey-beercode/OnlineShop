b18d0e42669892a2a5b34d4bfd9910267fd016b5
KiSSel3 <andreyk042004@gmail.com>
2024-06-25 16:37:04 +0300

    Update program.cs

--- OnlineShop/OnlineShop.Presentation/Program.cs
+++ OnlineShop/OnlineShop.Presentation/Program.cs
@@ -1,22 +1,16 @@
 using OnlineShop.Extensions;
 
-internal class Program
-{
-    public static void Main(string[] args)
-    {
-        var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
         
-        builder.AddAuthentication();
-        builder.AddDatabase();
-        builder.AddMapping();
-        builder.AddServices();
-        builder.AddValidation();
-        builder.AddSwaggerDocumentation();
+builder.AddAuthentication();
+builder.AddDatabase();
+builder.AddMapping();
+builder.AddServices();
+builder.AddValidation();
+builder.AddSwaggerDocumentation();
         
-        var app = builder.Build();
-        app.AddSwagger();
-        app.AddApplicationMiddleware();
+var app = builder.Build();
+app.AddSwagger();
+app.AddApplicationMiddleware();
 
-        app.Run();
-    }
-}
+app.Run();
